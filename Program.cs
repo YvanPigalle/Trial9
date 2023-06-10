57aa93b1a192129f22c42315bd888240fc64cfff
YvanPigalle <vanquishracer@gmail.com>
2023-06-10 13:41:59 +0300

    done

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ void RecuNum(int m, int count)
 
 //Задайте значения M и N.
 //Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-
+/*
 void SumBtwMAndN(int m, int n)
 {
     Console.Write(SumMN(m - 1, n));
@@ -43,3 +43,34 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 SumBtwMAndN(m, n);
+*/
+//Задача: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//Даны два неотрицательных числа m и n.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanF (m, n);
+void AkkermanF (int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
